whatever60/cyberduck

commit af37abdac49453bf41845dafacd8692c6c33e345

Fix #17680.

osx/src/main/java/ch/cyberduck/ui/cocoa/callback/PromptCertificateIdentityCallback.java
@@ -16,6 +16,7 @@ package ch.cyberduck.ui.cocoa.callback;
  */
 
 import ch.cyberduck.binding.AlertRunner;
+import ch.cyberduck.binding.Outlet;
 import ch.cyberduck.binding.ProxyController;
 import ch.cyberduck.binding.SheetController;
 import ch.cyberduck.binding.WindowController;
@@ -31,10 +32,10 @@ import ch.cyberduck.core.keychain.SFChooseIdentityPanel;
 import ch.cyberduck.core.keychain.SecIdentityRef;
 import ch.cyberduck.core.keychain.SecPolicyRef;
 import ch.cyberduck.core.keychain.SecurityFunctions;
-import ch.cyberduck.core.threading.DefaultMainAction;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.rococoa.Rococoa;
 
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
@@ -65,25 +66,32 @@ public class PromptCertificateIdentityCallback implements CertificateIdentityCal
     @Override
     public X509Certificate prompt(final String hostname, final List<X509Certificate> certificates) throws ConnectionCanceledException {
         final AtomicReference<SFChooseIdentityPanel> ref = new AtomicReference<>();
-        controller.invoke(new DefaultMainAction() {
-            @Override
-            public void run() {
-                ref.set(SFChooseIdentityPanel.sharedChooseIdentityPanel());
-            }
-        }, true);
-        final SFChooseIdentityPanel panel = ref.get();
-        panel.setDomain(hostname);
-        final SecPolicyRef policyRef = SecurityFunctions.library.SecPolicyCreateSSL(true, hostname);
-        panel.setPolicies(policyRef);
-        FoundationKitFunctions.library.CFRelease(policyRef);
-        panel.setShowsHelp(false);
-        panel.setAlternateButtonTitle(LocaleFactory.localizedString("Disconnect"));
-        panel.setInformativeText(MessageFormat.format(LocaleFactory.localizedString(
-                "The server requires a certificate to validate your identity. Select the certificate to authenticate yourself to {0}."), hostname));
         final NSArray identities = KeychainCertificateStore.toDEREncodedCertificates(certificates);
-        final int option = controller.alert(new SheetController.NoBundleSheetController(panel), new AlertRunner() {
+        final int option = controller.alert(new SheetController.NoBundleSheetController() {
+            @Outlet
+            private SFChooseIdentityPanel panel;
+
+            @Override
+            public void loadBundle() {
+                panel = SFChooseIdentityPanel.sharedChooseIdentityPanel();
+                panel.setDomain(hostname);
+                final SecPolicyRef policyRef = SecurityFunctions.library.SecPolicyCreateSSL(true, hostname);
+                panel.setPolicies(policyRef);
+                FoundationKitFunctions.library.CFRelease(policyRef);
+                panel.setShowsHelp(false);
+                panel.setAlternateButtonTitle(LocaleFactory.localizedString("Disconnect"));
+                panel.setInformativeText(MessageFormat.format(LocaleFactory.localizedString(
+                        "The server requires a certificate to validate your identity. Select the certificate to authenticate yourself to {0}."), hostname));
+            }
+
+            @Override
+            public NSWindow window() {
+                return panel;
+            }
+        }, new AlertRunner() {
             @Override
             public void alert(final NSWindow sheet, final SheetCallback callback) {
+                final SFChooseIdentityPanel panel = Rococoa.cast(sheet, SFChooseIdentityPanel.class);
                 if(null == window) {
                     callback.callback(panel.runModalForIdentities_message(identities, null).intValue());
                 }
@@ -98,6 +106,7 @@ public class PromptCertificateIdentityCallback implements CertificateIdentityCal
         switch(option) {
             case SheetCallback.DEFAULT_OPTION:
                 // Use the identity method to obtain the identity chosen by the user.
+                final SFChooseIdentityPanel panel = ref.get();
                 final SecIdentityRef identityRef = panel.identity();
                 if(null == identityRef) {
                     log.warn("No identity selected for {}", hostname);

binding/src/main/java/ch/cyberduck/binding/SheetController.java
@@ -104,13 +104,12 @@ public abstract class SheetController extends WindowController implements InputV
      * Implementation with no bundle loaded but window reference only
      */
     public static class NoBundleSheetController extends SheetController {
-        public NoBundleSheetController(final NSWindow window) {
-            this(window, InputValidator.disabled);
+        public NoBundleSheetController() {
+            this(InputValidator.disabled);
         }
 
-        public NoBundleSheetController(final NSWindow window, final InputValidator callback) {
+        public NoBundleSheetController(final InputValidator callback) {
             super(callback);
-            this.setWindow(window);
         }
 
         @Override

osx/src/main/java/ch/cyberduck/ui/cocoa/callback/PromptCertificateTrustCallback.java
@@ -16,6 +16,7 @@ package ch.cyberduck.ui.cocoa.callback;
  */
 
 import ch.cyberduck.binding.AlertRunner;
+import ch.cyberduck.binding.Outlet;
 import ch.cyberduck.binding.ProxyController;
 import ch.cyberduck.binding.SheetController;
 import ch.cyberduck.binding.WindowController;
@@ -30,14 +31,13 @@ import ch.cyberduck.core.keychain.SFCertificateTrustPanel;
 import ch.cyberduck.core.keychain.SecPolicyRef;
 import ch.cyberduck.core.keychain.SecTrustRef;
 import ch.cyberduck.core.keychain.SecurityFunctions;
-import ch.cyberduck.core.threading.DefaultMainAction;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.rococoa.Rococoa;
 
 import java.security.cert.X509Certificate;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
 import com.sun.jna.ptr.PointerByReference;
 
@@ -67,22 +67,28 @@ public class PromptCertificateTrustCallback implements CertificateTrustCallback 
         final PointerByReference reference = new PointerByReference();
         SecurityFunctions.library.SecTrustCreateWithCertificates(KeychainCertificateStore.toDEREncodedCertificates(certificates), policyRef, reference);
         final SecTrustRef trustRef = new SecTrustRef(reference.getValue());
-        final AtomicReference<SFCertificateTrustPanel> ref = new AtomicReference<>();
-        controller.invoke(new DefaultMainAction() {
-            @Override
-            public void run() {
-                ref.set(SFCertificateTrustPanel.sharedCertificateTrustPanel());
-            }
-        }, true);
-        final SFCertificateTrustPanel panel = ref.get();
-        panel.setInformativeText(null);
-        panel.setAlternateButtonTitle(LocaleFactory.localizedString("Disconnect"));
-        panel.setPolicies(policyRef);
-        panel.setShowsHelp(true);
         log.debug("Display trust panel for controller {}", controller);
-        final int option = controller.alert(new SheetController.NoBundleSheetController(panel), new AlertRunner() {
+        final int option = controller.alert(new SheetController.NoBundleSheetController() {
+            @Outlet
+            private SFCertificateTrustPanel panel;
+
+            @Override
+            public void loadBundle() {
+                panel = SFCertificateTrustPanel.sharedCertificateTrustPanel();
+                panel.setInformativeText(null);
+                panel.setAlternateButtonTitle(LocaleFactory.localizedString("Disconnect"));
+                panel.setPolicies(policyRef);
+                panel.setShowsHelp(true);
+            }
+
+            @Override
+            public NSWindow window() {
+                return panel;
+            }
+        }, new AlertRunner() {
             @Override
             public void alert(final NSWindow sheet, final SheetCallback callback) {
+                final SFCertificateTrustPanel panel = Rococoa.cast(sheet, SFCertificateTrustPanel.class);
                 if(null == window) {
                     callback.callback(panel.runModalForTrust_message(trustRef, null).intValue());
                 }

osx/src/main/java/ch/cyberduck/ui/cocoa/callback/PromptLoginCallback.java
@@ -24,7 +24,6 @@ import ch.cyberduck.binding.WindowController;
 import ch.cyberduck.binding.application.NSOpenPanel;
 import ch.cyberduck.binding.application.NSWindow;
 import ch.cyberduck.binding.application.SheetCallback;
-import ch.cyberduck.binding.foundation.NSObject;
 import ch.cyberduck.binding.foundation.NSURL;
 import ch.cyberduck.core.Credentials;
 import ch.cyberduck.core.Host;
@@ -58,9 +57,6 @@ public class PromptLoginCallback extends PromptPasswordCallback implements Login
     private final ProxyController controller;
     private final NSWindow window;
 
-    @Outlet
-    private NSOpenPanel select;
-
     public PromptLoginCallback(final ProxyController controller) {
         super(controller);
         this.controller = controller;
@@ -72,6 +68,7 @@ public class PromptLoginCallback extends PromptPasswordCallback implements Login
 
         }
     }
+
     @Override
     public void await(final CountDownLatch signal, final Host bookmark, final String title, final String message) throws ConnectionCanceledException {
         log.debug("Display progress alert for {}", bookmark);
@@ -116,10 +113,24 @@ public class PromptLoginCallback extends PromptPasswordCallback implements Login
     }
 
     public Local select(final Local identity) throws LoginCanceledException {
-        final int option = controller.alert(new SheetController.NoBundleSheetController(select), new AlertRunner() {
+        final SheetController.NoBundleSheetController sheet = new SheetController.NoBundleSheetController() {
+            @Outlet
+            private NSOpenPanel select;
+
+            @Override
+            public void loadBundle() {
+                select = NSOpenPanel.openPanel();
+            }
+
+            @Override
+            public NSWindow window() {
+                return select;
+            }
+        };
+        final int option = controller.alert(sheet, new AlertRunner() {
             @Override
             public void alert(final NSWindow sheet, final SheetCallback callback) {
-                select = NSOpenPanel.openPanel();
+                final NSOpenPanel select = Rococoa.cast(sheet, NSOpenPanel.class);
                 select.setCanChooseDirectories(false);
                 select.setCanChooseFiles(true);
                 select.setAllowsMultipleSelection(false);
@@ -137,9 +148,9 @@ public class PromptLoginCallback extends PromptPasswordCallback implements Login
             }
         });
         if(option == SheetCallback.DEFAULT_OPTION) {
-            final NSObject url = select.URLs().lastObject();
+            final NSURL url = Rococoa.cast(Rococoa.cast(sheet.window(), NSOpenPanel.class).URLs().lastObject(), NSURL.class);
             if(url != null) {
-                final Local selected = LocalFactory.get(Rococoa.cast(url, NSURL.class).path());
+                final Local selected = LocalFactory.get(url.path());
                 return selected.setBookmark(FilesystemBookmarkResolverFactory.get().create(selected));
             }
         }

commit 40c1a4c2c11f8b1ce29e1edaf7f1e68e103a9c4a

Add missing assignment. Regression from af37abda.

osx/src/main/java/ch/cyberduck/ui/cocoa/callback/PromptCertificateIdentityCallback.java
@@ -74,7 +74,7 @@ public class PromptCertificateIdentityCallback implements CertificateIdentityCal
 
             @Override
             public void loadBundle() {
-                panel = SFChooseIdentityPanel.sharedChooseIdentityPanel();
+                ref.set(panel = SFChooseIdentityPanel.sharedChooseIdentityPanel());
                 panel.setDomain(hostname);
                 final SecPolicyRef policyRef = SecurityFunctions.library.SecPolicyCreateSSL(true, hostname);
                 panel.setPolicies(policyRef);